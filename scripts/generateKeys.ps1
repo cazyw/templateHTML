4a645e766ba9d73ebb6974c95a862b9cb13c3799
cazyw <kazeisc@gmail.com>
2018-11-04 22:13:25 +1100

    Add script to generate private and public RSA keys, x509 certificate and base64 version of the pfx, in powershell

---
+++ scripts/generateKeys.ps1
@@ -0,0 +1,27 @@
+if(!(Test-Path "Keys")){
+  New-Item -ItemType "Directory" -path "Keys"
+}
+$pwd = pwd
+
+Set-Location "$pwd\Keys"
+Write-Host " === Creating a private key 'privatekey.pem' === "
+openssl genrsa -out privatekey.pem 2048
+
+Write-Host " === Creating a X509 certificate === "
+openssl req -new -x509 -key privatekey.pem -out publickey.cer -days 1825
+
+Write-Host " === Export your x509 certificate and private key to a pfx file 'public_privatekey.pfx' === "
+openssl pkcs12 -export -out public_privatekey.pfx -inkey privatekey.pem -in publickey.cer
+
+Write-Host " === Create public key 'publickey.pub' === "
+openssl rsa -in privatekey.pem -pubout > publickey.pub
+
+Write-Host " === Generating base64 'base64_public_privatekey.txt' === "
+$pwd = pwd
+$file = Get-Item "$pwd\public_privatekey.pfx"
+$bytes = [System.IO.File]::ReadAllBytes($file.FullName);
+$base64String = [System.Convert]::ToBase64String($bytes);
+[System.IO.File]::WriteAllText("$pwd\base64_public_privatekey.txt" ,$base64string);
+
+Set-Location ".."
+Write-Host " === Completed === "
