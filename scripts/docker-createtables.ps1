27c99ce6e1e48cf80a474792ee33da32ca5c9981
cazyw <kazeisc@gmail.com>
2019-01-19 15:10:38 +1100

    fix variables in create/delete dynamodb table scripts

--- scripts/docker-createtables.ps1
+++ scripts/docker-createtables.ps1
@@ -1,42 +1,46 @@
+# Create tables for local dynamodb tables
+# Prerequisites: AWS Tools for Powershell
+#   Install-Module -Name AWSPowerShell
 # https://aws.amazon.com/blogs/developer/creating-amazon-dynamodb-tables-with-powershell/
+# DynamoDB must be running
 
 param([string]$tablename="TestTable")
 
 $endpoint = "http://192.168.99.100:9000"
-$sitecoreReportingMasterTable = $tablename
+$localMasterTable = $tablename
 $region = "ap-southeast-2"
 
 Write-Host "Setting up local database table" -ForegroundColor "Yellow"
 
 $tableSchema = New-DDBTableSchema `
-            | Add-DDBKeySchema -KeyName "PageViewMonthId" -KeyType "HASH" -KeyDataType "S" `
-            | Add-DDBKeySchema -KeyName "SitecoreItemId" -KeyType "RANGE" -KeyDataType "S" `
+            | Add-DDBKeySchema -KeyName "Id" -KeyType "HASH" -KeyDataType "S" `
+            | Add-DDBKeySchema -KeyName "Name" -KeyType "RANGE" -KeyDataType "S" `
             | Add-DDBIndexSchema -Global `
-                        -IndexName "SitecoreItemIdPageViewMonthId" `
-                        -HashKeyName "SitecoreItemId" `
-                        -RangeKeyName "PageViewMonthId" `
+                        -IndexName "NameId" `
+                        -HashKeyName "Name" `
+                        -RangeKeyName "Id" `
                         -HashKeyDataType "S" `
                         -ProjectionType "ALL" `
                         -ReadCapacity 5 `
                         -WriteCapacity 5
 try {
-    Write-Host "Removing existing local dynamoDB table $sitecoreReportingMasterTable" -ForegroundColor "Yellow"
-    $removeTableResp = Remove-DDBTable -TableName $sitecoreReportingMasterTable `
+    Write-Host "Removing existing local dynamoDB table $localMasterTable" -ForegroundColor "Yellow"
+    $removeTableResp = Remove-DDBTable -TableName $localMasterTable `
                                       -EndpointUrl $endpoint `
                                       -Region $region `
                                       -Force
 
-    Write-Host "Removed existing local dynamoDB table $sitecoreReportingMasterTable" -ForegroundColor "Green"
+    Write-Host "Removed existing local dynamoDB table $localMasterTable" -ForegroundColor "Green"
 } catch {
-    Write-Host "Cannot remove local dynamoDB table $sitecoreReportingMasterTable / Table doesn't exist" -ForegroundColor "Yellow"
+    Write-Host "Cannot remove local dynamoDB table $localMasterTable / Table doesn't exist" -ForegroundColor "Yellow"
 }
 
-Write-Host "Creating local dynamoDB table $sitecoreReportingMasterTable" -ForegroundColor "Yellow"
-$createTableResp = New-DDBTable -TableName $sitecoreReportingMasterTable `
+Write-Host "Creating local dynamoDB table $localMasterTable" -ForegroundColor "Yellow"
+$createTableResp = New-DDBTable -TableName $localMasterTable `
                                 -Schema $tableSchema `
                                 -ReadCapacity  5 `
                                 -WriteCapacity 5 `
                                 -EndpointUrl $endpoint `
                                 -Region $region
-                                Write-Host "Created local dynamoDB table $sitecoreReportingMasterTable" -ForegroundColor "Green"
+                                Write-Host "Created local dynamoDB table $localMasterTable" -ForegroundColor "Green"
 
